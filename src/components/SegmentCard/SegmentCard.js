c3bbc3183a6a26f5211a251b2a544ef4164338f1
azezeB <azezebrajov@gmail.com>
2023-02-08 18:38:47 +0100

    Refactor Segmentcard UI

--- src/components/SegmentCard/SegmentCard.js
+++ src/components/SegmentCard/SegmentCard.js
@@ -1,26 +1,26 @@
 import { Card } from "react-bootstrap";
-import classes from "./SegmentCard.module.css"
+import classes from "./SegmentCard.module.css";
 
 const SegmentCard = (props) => {
-  console.log(props.baseUrl + `/${props.id}`)
+  console.log(props.baseUrl + `/${props.id}`);
 
   return (
-    <Card className={classes.Card} >
-      <Card.Header as="h5">{props.issue.name}</Card.Header>
-      <Card.Body className={classes.Body}>
+    <div className={classes.Card}>
+      <div className={classes.Body}>
+        <h5 className={classes.Title}>{props.issue.name}</h5>
         <video
           className={classes.Segment}
           src={props.baseUrl + `/segments/${props.id}`}
           controls
         ></video>
-        <div className={classes.Infos}>
-          <p>Minutaggio: </p>
-          <p>Origine: {props.video.name}</p>
-          <p>Secondo inizio: </p>
-          <p>Secondo fine: </p>
-        </div>
-      </Card.Body>
-    </Card>
+        <ul className={classes.Infos}>
+          <li>Minutaggio: 2:15</li>
+          <li>Origine: {props.video.name}</li>
+          <li>Secondo inizio: 10s</li>
+          <li>Secondo fine: 16s</li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
