b346c3a4ccaae9407d8cef202ddef2e8eaf66018
azezeB <azezebrajov@gmail.com>
2023-02-08 18:38:06 +0100

    Refactor Segmentcard UI

--- src/components/SegmentCard/SegmentCard.js
+++ src/components/SegmentCard/SegmentCard.js
@@ -7,13 +7,18 @@ const SegmentCard = (props) => {
   return (
     <Card className={classes.Card} >
       <Card.Header as="h5">{props.issue.name}</Card.Header>
-      <Card.Body>
-        <Card.Text></Card.Text>
+      <Card.Body className={classes.Body}>
         <video
           className={classes.Segment}
           src={props.baseUrl + `/segments/${props.id}`}
           controls
         ></video>
+        <div className={classes.Infos}>
+          <p>Minutaggio: </p>
+          <p>Origine: {props.video.name}</p>
+          <p>Secondo inizio: </p>
+          <p>Secondo fine: </p>
+        </div>
       </Card.Body>
     </Card>
   );
